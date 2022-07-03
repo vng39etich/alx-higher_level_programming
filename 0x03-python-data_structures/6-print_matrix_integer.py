51699bb329ce0a6923a0af0c33121acef9444e96
vng39etich <vng39etich@gmail.com>
2022-07-03 20:45:34 +0300

    Task 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     numrows = len(matrix)
     numcols = len(matrix[0])
-    for x in range(0, numrows):
-        for y in range(0, numcols):
-            if y == numcols - 1:
+    for i in range(0, numrows):
+        for j in range(0, numcols):
+            if j == numcols - 1:
                 print('{:d}'.format(matrix[i][j]), end='')
             else:
                 print('{:d}'.format(matrix[i][j]), end=' ')
